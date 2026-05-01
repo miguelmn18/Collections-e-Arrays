d514c5eb4f08b32e0228a7d1d6407840ef1ebc76
Miguel de Macedo Nunes <miguel.macedo@dcx.ufpb.br>
2026-04-30 21:07:35 -0300

    Exercicio1 -Usuário informar os números -A expressão é construída e mostrada ao usuário -Saída: Usuário vê o valor e a expressão que ele montou

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Vector;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> pilhaNumeros = new Stack<>();
+        Stack<String> operadores = new Stack<>();
+        char c;
+        int numerosPositivos = Integer.MAX_VALUE;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        operadores.add("+");
+        operadores.add("-");
+        operadores.add("*");
+        operadores.add("/");
+
+        //int numerosPositivos = Integer.MAX_VALUE;
+        //pilhaNumeros.add(numerosPositivos);
+        //System.out.println(numerosPositivos);
+        System.out.println(operadores);
+
+
+        System.out.println("Digite sua expressão: ");
+        String expressao = sc.nextLine();
+
+        for (int i = 0; i < expressao.length(); i++) {
+            c = expressao.charAt(i);
+            if (Character.isDigit(c)) {
+                StringBuilder numeroCompleto = new StringBuilder();
+                while (i < expressao.length() && Character.isDigit(expressao.charAt(i))) {
+                    numeroCompleto.append(expressao.charAt(i));
+                    i++;
+                }
+                int valor = Integer.parseInt(numeroCompleto.toString());
+                pilhaNumeros.add(valor);
+                System.out.println("Numero adicionado: " + valor);
+            }
         }
+        System.out.println("Pilha final: " + pilhaNumeros);
     }
 }
