fc9554c5f27fd17fb01095d33963dc55c39c83fe
Miguel de Macedo Nunes <miguel.macedo@dcx.ufpb.br>
2026-05-01 18:37:34 -0300

    Exercicio1 -Usuário informar os números -A expressão é construída e mostrada ao usuário -Saída: Usuário vê o valor e a expressão que ele montou

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.List;
-import java.util.Scanner;
-import java.util.Stack;
-import java.util.Vector;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -18,28 +15,80 @@ public class Main {
         operadores.add("*");
         operadores.add("/");
 
-        //int numerosPositivos = Integer.MAX_VALUE;
-        //pilhaNumeros.add(numerosPositivos);
-        //System.out.println(numerosPositivos);
         System.out.println(operadores);
 
 
         System.out.println("Digite sua expressão: ");
         String expressao = sc.nextLine();
+        List<String> tokens = tokenize(expressao);
+        System.out.println("Tokens: " + tokens);
+        System.out.println("=================================");
+        List<String> postFix = toPostFix(tokens);
+        System.out.println("Expressão em pos fixa: " + postFix);
+        System.out.println("=========================");
 
-        for (int i = 0; i < expressao.length(); i++) {
-            c = expressao.charAt(i);
-            if (Character.isDigit(c)) {
-                StringBuilder numeroCompleto = new StringBuilder();
-                while (i < expressao.length() && Character.isDigit(expressao.charAt(i))) {
-                    numeroCompleto.append(expressao.charAt(i));
-                    i++;
-                }
-                int valor = Integer.parseInt(numeroCompleto.toString());
-                pilhaNumeros.add(valor);
-                System.out.println("Numero adicionado: " + valor);
+
+
+    }
+
+    public static List<String> tokenize(String expressao) {
+        List<String> tokens = new ArrayList<>();
+        String[] parts = expressao.split("(?<=[-+*/()])|(?=[-+*/()])");
+        for (String part : parts) {
+
+            String token = part.trim();
+            if (!token.isEmpty()) {
+                tokens.add(token);
             }
         }
-        System.out.println("Pilha final: " + pilhaNumeros);
+        return tokens;
     }
-}
+
+    public static List<String> toPostFix(List<String> tokens) {
+        List<String> output = new ArrayList<>();
+        Stack<String> operadores = new Stack<>();
+
+        for (String token : tokens) {
+            if (token.matches("\\d+")) {
+                //Se for número, vai direto pra saída
+                output.add(token);
+            } else if (token.equals("(")) {
+                //Abre parentêses vai para a pilha de operadores
+                operadores.push(token);
+            } else if (token.equals(")")) {
+                //Fecha parantêses: desempilha até encontrar ")
+                while (!operadores.isEmpty() && !operadores.peek().equals("(")) {
+                    output.add(operadores.pop());
+                }
+                operadores.push(token);
+            }
+        }
+        return output;
+    }
+
+
+
+            /* Desempilha operadores restantes
+            while (!operadores.isEmpty()) {
+                output.add(operadores.pop());
+            }
+
+            return output;
+        }*/
+
+
+
+
+
+        public static boolean isOperator (String token){
+            return token.equals("+") || token.equals("-") ||
+                    token.equals("x") || token.equals("/");
+        }
+        private static int precedence (String op){
+            if(op.equals("+") || op.equals("-")) return 1;
+            if(op.equals("*") || op.equals("/"))return 2;
+            return 0;
+        }
+
+
+    }
